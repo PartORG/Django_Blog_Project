2e698650b946936eef41c2b8d13f2f15c15f02c8
Ievgen Perederieiev <peredereev.ievgen@gmail.com>
2023-11-30 18:26:47 +0100

    added site files.

---
+++ my_site/blog/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Tag, Author, Post, Comment
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author", "date", "tags",)
+    list_display = ("title", "author", "date",)
+    prepopulated_fields = {"slug": ("title",)}
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "post")
+
+
+# Register your models here.
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
+admin.site.register(Comment, CommentAdmin)
